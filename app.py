284449be31518fa348ae15408ab65d84ecaf0ed2
Ben Cheng <bencheng@oursky.com>
2023-04-05 19:13:11 +0800

    Preserve thread respond

--- app.py
+++ app.py
@@ -14,12 +14,19 @@ openai.api_base = os.environ["OPENAI_API_BASE"]
 openai.api_key = os.environ["OPENAI_API_KEY"]
 openai.api_version = "2023-03-15-preview"
 
-@app.event("app_mention")
-def event_test(event, say):
+threads = {} 
+
+def handle_prompt(ts, text, say):
+    if ts not in threads:
+        threads[ts] = [ {"role":"system", "content": "You are an AI assistant."} ]
+
+    threads[ts].append({"role":"user", "content": text})
+
+    print(threads[ts])
+
     response = openai.ChatCompletion.create(
         engine="oursky-demo",
-        messages = [ {"role":"system", "content": "You are an AI assistant."},
-                     {"role": "user", "content": event["text"]}],
+        messages = threads[ts],
         temperature=0.5,
         max_tokens=800,
         top_p=0.95,
@@ -27,7 +34,28 @@ def event_test(event, say):
         presence_penalty=0,
         stop=None
     )
-    say(response['choices'][0]['message']['content'])
+
+    response = response['choices'][0]['message']['content']
+    threads[ts].append({"role":"assistant", "content":response})
+
+    say(response, thread_ts=ts)
+
+@app.event("app_mention")
+def event_prompt(event, say):
+    ts = event["ts"]
+
+    handle_prompt(ts, event["text"], say)
+
+
+@app.event("message")
+def event_reply(event, say):
+    ts = event.get("thread_ts")
+
+    if ts is None:
+        return
+
+    if ts in threads:
+        handle_prompt(ts, event["text"], say)
 
 
 # Start your app
